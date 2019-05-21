477140c9fb9cc182658e486ad1b4988f7b736f45
Adam Parker <adam@theadamparker.com>
2019-05-21 15:10:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# OC Lift Landing Page
+
+This repo manages the landing page for OCP's Lift program.
+
+## Structure
+
+The landing page is an `index.html` file hosted on Github pages.
+
+## Development
+
+While the structure is bare bones, there are still some basic build processes involved. The page uses [Gulp](https://gulpjs.com/), so Node.js will need to be installed before you're ready to start contributing.
+
+After cloning the repo to your computer `cd` to the directory and run `npm install`. This will install Gulp and its dependencies. Once everything is installed run `gulp` and BrowserSync will launch the page, and watch for changes to the SCSS files.
+
+### CSS
+
+All styles are created in `main.scss` and exported to `main.css` with Gulp. When editing any CSS make sure Gulp is running in your terminal.
+
+## Contributing
+
+Please report issues directly on Github. 
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+// let postcss = require('gulp-postcss');
+
+function style() {
+  return gulp.src('build/scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('build/scss/*.scss', style);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('build/js/*.js').on('change', browserSync.reload);
+}
+
+// gulp.task('css', () => {
+//     return gulp.src('build/css/*.css')
+//         .pipe(postcss())
+//         .pipe(gulp.dest('dist/css'));
+// });
+
+
+// gulp.task('default', gulp.parallel('sass', 'minify-css'));
+
+exports.style = style;
+exports.watch = watch;
